361e52b8467b684b7ea7ff2408dd9d5aa1481036
Ali <ali-ashoori@outlook.com>
2021-06-03 13:22:25 +0100

    added ReportValueCellsCalculatorValidator

---
+++ TechnicalTest/TechnicalTest/Server/Validators/IReportValueCellsCalculatorValidator.cs
@@ -0,0 +1,17 @@
+﻿using OfficeOpenXml;
+
+namespace TechnicalTest.Server
+{
+    public interface IReportValueCellsCalculatorValidator
+    {
+        void Validate(ExcelWorksheet worksheet);
+    }
+
+    public class ReportValueCellsCalculatorValidator : IReportValueCellsCalculatorValidator
+    {
+        public void Validate(ExcelWorksheet worksheet)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
