8fd39d0019d569256423bc348b1de87923352158
Ali <ali-ashoori@outlook.com>
2021-06-04 00:54:25 +0100

    added AppServiceRegistrationTests

---
+++ TechnicalTest/Tests/TechnicalTest.Tests/AppServiceRegistrationTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.CodeAnalysis;
+using TechnicalTest.Server;
+
+namespace TechnicalTest.Tests
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class AppServiceRegistrationTests
+    {
+        [TestMethod]
+        public void Register_HappyScenario_RegistersAllServices()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            // Act
+            services.RegisterAppServices();
+
+            // Assert
+            services.Should().HaveCount(11);
+        }
+    }
+}
