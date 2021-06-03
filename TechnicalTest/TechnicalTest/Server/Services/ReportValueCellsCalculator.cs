8fae4a4bf4cf6467bcb608a9acf9f7fb7065103d
Ali <ali-ashoori@outlook.com>
2021-06-03 13:22:13 +0100

    implemented ReportValueCellsCalculator

---
+++ TechnicalTest/TechnicalTest/Server/Services/ReportValueCellsCalculator.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using OfficeOpenXml;
+using TechnicalTest.Shared;
+using System.Linq;
+
+namespace TechnicalTest.Server.Services
+{
+    public interface IReportValueCellsCalculator
+    {
+        IOrderedEnumerable<ReportValueCell> Calculate(ExcelWorksheet worksheet);
+    }
+
+    public class ReportValueCellsCalculator : IReportValueCellsCalculator
+    {
+        private readonly ILogger<ReportValueCellsCalculator> _logger;
+        private readonly IReportValueCellsCalculatorValidator _validator;
+
+        public ReportValueCellsCalculator(ILogger<ReportValueCellsCalculator> logger, IReportValueCellsCalculatorValidator validator)
+        {
+            _logger = logger.NotNull();
+            _validator = validator.NotNull();
+        }
+
+        public IOrderedEnumerable<ReportValueCell> Calculate(ExcelWorksheet worksheet)
+        {
+            _validator.Validate(worksheet);
+
+            var targetCells = Enumerable.Empty<ReportValueCell>();
+
+            var values = worksheet.Cells.Value as object[,];
+
+            for (int i = worksheet.Dimension.Start.Row; i < worksheet.Dimension.End.Row; i++)
+            {
+                for (int j = worksheet.Dimension.Start.Column; j < worksheet.Dimension.End.Column; j++)
+                {
+                    if (values[i, j] == null) continue;
+
+                    var currentCellValue = values[i, j].ToString();
+
+                    if (!string.IsNullOrWhiteSpace(currentCellValue) &&
+                        currentCellValue.ToCharArray().All(c => char.IsDigit(c)))
+                    {
+                        var element = new ReportValueCell { Value = currentCellValue, Row = i, Column = j };
+                        targetCells = targetCells.Append(element);
+                    }
+                }
+            }
+
+            return targetCells.OrderBy(cell => cell.Row);
+        }
+    }
+}
