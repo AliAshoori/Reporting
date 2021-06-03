b8305a21fbdd7acd1c48f2b51e9307e3050ab8ce
Ali <ali-ashoori@outlook.com>
2021-06-03 14:31:03 +0100

    implemented ReportValueCellsCalculatorValidator

--- TechnicalTest/TechnicalTest/Server/Validators/IReportValueCellsCalculatorValidator.cs
+++ TechnicalTest/TechnicalTest/Server/Validators/IReportValueCellsCalculatorValidator.cs
@@ -1,4 +1,7 @@
-﻿using OfficeOpenXml;
+﻿using Microsoft.Extensions.Logging;
+using OfficeOpenXml;
+using System;
+using TechnicalTest.Shared;
 
 namespace TechnicalTest.Server
 {
@@ -9,9 +12,24 @@ namespace TechnicalTest.Server
 
     public class ReportValueCellsCalculatorValidator : IReportValueCellsCalculatorValidator
     {
+        private readonly ILogger<ReportValueCellsCalculatorValidator> _logger;
+
+        public ReportValueCellsCalculatorValidator(ILogger<ReportValueCellsCalculatorValidator> logger)
+        {
+            _logger = logger.NotNull();
+        }
+
         public void Validate(ExcelWorksheet worksheet)
         {
-            throw new System.NotImplementedException();
+            if (worksheet == null)
+            {
+                throw new ArgumentNullException(nameof(worksheet));
+            }
+
+            if (worksheet.Cells == null)
+            {
+                throw new InvalidOperationException($"Worksheet {worksheet.Name} has not Cells added to it");
+            }
         }
     }
 }
