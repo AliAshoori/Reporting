e7d40c21318aeb652805e3891224a3538e674452
Ali <ali-ashoori@outlook.com>
2021-06-03 22:53:04 +0100

    fixed unit test

--- TechnicalTest/Tests/TechnicalTest.Tests/Parsers/DataTableToRawHtmlParserTests.cs
+++ TechnicalTest/Tests/TechnicalTest.Tests/Parsers/DataTableToRawHtmlParserTests.cs
@@ -25,17 +25,16 @@ namespace TechnicalTest.Tests
             table.Columns.Add("Dosage", typeof(int));
             table.Columns.Add("Drug", typeof(string));
             table.Columns.Add("Diagnosis", typeof(string));
-            table.Columns.Add("Date", typeof(DateTime));
 
-            table.Rows.Add(25, "Drug A", "Disease A", DateTime.Now);
-            table.Rows.Add(50, "Drug Z", "Problem Z", DateTime.Now);
-            table.Rows.Add(10, "Drug Q", "Disorder Q", DateTime.Now);
-            table.Rows.Add(21, "Medicine A", "Diagnosis A", DateTime.Now);
+            table.Rows.Add(25, "Drug A", "Disease A");
+            table.Rows.Add(50, "Drug Z", "Problem Z");
+            table.Rows.Add(10, "Drug Q", "Disorder Q");
+            table.Rows.Add(21, "Medicine A", "Diagnosis A");
 
             var parser = new DataTableToRawHtmlParser(mockLogger.Object, mockValidator.Object);
 
             var expected =
-                "<table class='report-table'><tr class='report-table-row'><td class='report-column'>Dosage</td><td class='report-column'>Drug</td><td class='report-column'>Diagnosis</td><td class='report-column'>Date</td></tr><tr class='report-table-row'><td>25</td><td class='header-style'>Drug A</td><td class='header-style'>Disease A</td><td>03/06/2021 22:20:45</td></tr><tr class='report-table-row'><td>50</td><td class='header-style'>Drug Z</td><td class='header-style'>Problem Z</td><td>03/06/2021 22:20:45</td></tr><tr class='report-table-row'><td>10</td><td class='header-style'>Drug Q</td><td class='header-style'>Disorder Q</td><td>03/06/2021 22:20:45</td></tr><tr class='report-table-row'><td>21</td><td class='header-style'>Medicine A</td><td class='header-style'>Diagnosis A</td><td>03/06/2021 22:20:45</td></tr></table>";
+                "<table class='report-table'><tr class='report-table-row'><td class='report-column'>Dosage</td><td class='report-column'>Drug</td><td class='report-column'>Diagnosis</td></tr><tr class='report-table-row'><td>25</td><td class='header-style'>Drug A</td><td class='header-style'>Disease A</td></tr><tr class='report-table-row'><td>50</td><td class='header-style'>Drug Z</td><td class='header-style'>Problem Z</td></tr><tr class='report-table-row'><td>10</td><td class='header-style'>Drug Q</td><td class='header-style'>Disorder Q</td></tr><tr class='report-table-row'><td>21</td><td class='header-style'>Medicine A</td><td class='header-style'>Diagnosis A</td></tr></table>";
 
             // Act
             var actual = parser.Parse(table);
