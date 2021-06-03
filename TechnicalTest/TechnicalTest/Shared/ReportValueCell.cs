49688ff29760aac15420db92ab33f4493a233364
Ali <ali-ashoori@outlook.com>
2021-06-03 13:22:38 +0100

    added ReportValueCell model class

---
+++ TechnicalTest/TechnicalTest/Shared/ReportValueCell.cs
@@ -0,0 +1,11 @@
+﻿namespace TechnicalTest.Shared
+{
+    public class ReportValueCell
+    {
+        public string Value { get; set; }
+
+        public int Row { get; set; }
+
+        public int Column { get; set; }
+    }
+}
