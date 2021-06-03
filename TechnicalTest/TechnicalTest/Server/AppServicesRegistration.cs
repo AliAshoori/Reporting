437b8dffa3bd272a1f70ba62c5179ad01be5df2e
Ali <ali-ashoori@outlook.com>
2021-06-04 00:41:03 +0100

    registered services

---
+++ TechnicalTest/TechnicalTest/Server/AppServicesRegistration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TechnicalTest.Server.Services;
+
+namespace TechnicalTest.Server
+{
+    public static class AppServicesRegistration
+    {
+        public static void RegisterAppServices(this IServiceCollection services)
+        {
+            // parsers
+            services.AddScoped<IAppXmlParser, AppXmlParser>();
+            services.AddScoped<IDataTableToRawHtmlParser, DataTableToRawHtmlParser>();
+            services.AddScoped<IExcelToDataTableParser, ExcelToDataTableParser>();
+
+            // validators
+            services.AddScoped<IDataTableToRawHtmlParserValidator, DataTableToRawHtmlParserValidator>();
+            services.AddScoped<IExcelFileValidator, ExcelFileValidator>();
+            services.AddScoped<IReportValueCellsCalculatorValidator, ReportValueCellsCalculatorValidator>();
+            services.AddScoped<IReportValuesToExcelSheetMergerValidator, ReportValuesToExcelSheetMergerValidator>();
+            services.AddScoped<IXmlFileValidator, XmlFileValidator>();
+
+            // services
+            services.AddScoped<IReportGenerator, ReportGenerator>();
+            services.AddScoped<IReportValueCellsCalculator, ReportValueCellsCalculator>();
+            services.AddScoped<IReportValuesToExcelSheetMerger, ReportValuesToExcelSheetMerger>();
+        }
+    }
+}
