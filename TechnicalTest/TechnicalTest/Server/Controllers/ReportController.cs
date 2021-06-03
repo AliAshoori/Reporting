00ec1f99f1e5e3605fccd90afe7e332d347f22ff
Ali <ali-ashoori@outlook.com>
2021-06-04 00:35:03 +0100

    init the ui

---
+++ TechnicalTest/TechnicalTest/Server/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TechnicalTest.Server.Services;
+using TechnicalTest.Shared;
+
+namespace TechnicalTest.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReportController : ControllerBase
+    {
+        private readonly IReportGenerator _reportGenerator;
+
+        public ReportController(IReportGenerator reportGenerator)
+        {
+            _reportGenerator = reportGenerator.NotNull();
+        }
+
+        [HttpGet("Generate")]
+        public async Task<IActionResult> GenerateAsync() => Accepted(await _reportGenerator.GenerateAsync());
+    }
+}
